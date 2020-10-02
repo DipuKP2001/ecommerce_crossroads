f32c4f11d1d979f340805459c21133560a17888b
DipuKP2001 <dipukp602@gmail.com>
2020-10-02 13:07:59 +0530

    part 4

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,9 +1,15 @@
+const collection = require('../config/collection')
 var db = require('../config/connection')
 module.exports = {
     addProduct:(product,callback)=>{
-        console.log(product)
         db.get().collection('product').insertOne(product).then((data)=>{
             callback(data.ops[0]._id)
         })
-    }
+    },
+    getAllProducts:()=>{
+        return new Promise(async(resolve,reject)=>{
+            let products =await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolve(products)
+        })
+    },
 }
